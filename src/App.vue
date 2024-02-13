13289d6a3f34256f12913d9a2852b0b514ef1964
George Washington <nzologamer@gmail.com>
2024-02-13 18:42:42 +0600

    firstcom

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <div class=" bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-10">
+    <Header />
+
+    <h1>mt.washingtonn</h1>
+  </div>
+</template>
+
+<style></style>
